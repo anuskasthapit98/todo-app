70922753a6e6f0c19ae99951e0a5c5ac5580a604
Anuska Sthapit <anuska.sthapit12@gmail.com>
2021-07-18 09:51:45 +0545

    todo app

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include
+from .mypaginations import MyCursorPagination
 
-
-from .views import DesignationListAPIView
+from .views import DesignationListAPIView, TodoListCreate, TodoUpdateDelete
 
 urlpatterns = [
     path("designations/", DesignationListAPIView.as_view(), name="designations-list"),
 
+    path('todo/', TodoListCreate.as_view()),
+    path('todo/<int:pk>/', TodoUpdateDelete.as_view()),
 
 ]
--- api/views.py
+++ api/views.py
@@ -1,14 +1,31 @@
 
 from rest_framework.generics import ListAPIView
 from rest_framework.response import Response
+from rest_framework import generics
 
-from dashboard.models import Designation
-from .serializers import DesignationSerializer
-
+from dashboard.models import Designation, Todo
+from .serializers import DesignationSerializer, TodoSerializer
+from .mypaginations import MyCursorPagination
 
 class DesignationListAPIView(ListAPIView):
     queryset = Designation.objects.all()
     serializer_class = DesignationSerializer
+
+    def get_queryset(self):
+        return super().get_queryset().filter(deleted_at__isnull=True)
+
+class TodoListCreate( generics.ListCreateAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
+    pagination_class = MyCursorPagination
+
+    def get_queryset(self):
+        return super().get_queryset().filter(deleted_at__isnull=True)
+
+class TodoUpdateDelete( generics.RetrieveUpdateDestroyAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
+    pagination_class = MyCursorPagination
 
     def get_queryset(self):
         return super().get_queryset().filter(deleted_at__isnull=True)
